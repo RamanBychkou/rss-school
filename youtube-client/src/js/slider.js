76793408d6480dff886e0086a5c6c21243f349e3
Roman Bychkov <romabrest1992@gmail.com>
2018-11-25 02:02:59 +0300

    feat: add tooltip color

--- youtube-client/src/js/slider.js
+++ youtube-client/src/js/slider.js
@@ -4,24 +4,30 @@ module.exports = function slider(step, numbersSlide) {
   let margin = 0;
   const coordinate = {};
   function nextSlide(numbersSlide) {
+    document.querySelector('.active').removeAttribute('class');
     if (currentSlide === numbersSlide) {
       currentSlide = 1;
       margin = 0;
+      document.querySelector('.slide_1 > input').setAttribute('class', 'active');
       document.querySelector('.youtubeSlider > .videoSlide:first-child').style.marginLeft = '0px';
     } else {
       margin += step;
       currentSlide += stepCurrentSlide;
+      document.querySelector(`.toolTipWrapper ul li:nth-child(${currentSlide}) input`).setAttribute('class', 'active');
       document.querySelector('.youtubeSlider > .videoSlide:first-child').style.marginLeft = `-${margin}px`;
     }
   }
   function prevSlide(numbersSlide) {
+    document.querySelector('.active').removeAttribute('class');
     if (currentSlide === 1) {
       currentSlide = numbersSlide;
       margin = step * (numbersSlide - 1);
+      document.querySelector('.toolTipWrapper ul li:last-child > input').setAttribute('class', 'active');
       document.querySelector('.youtubeSlider > .videoSlide:first-child').style.marginLeft = `-${margin}px`;
     } else {
       margin -= step;
       currentSlide -= stepCurrentSlide;
+      document.querySelector(`.toolTipWrapper ul li:nth-child(${currentSlide}) input`).setAttribute('class', 'active');
       document.querySelector('.youtubeSlider > .videoSlide:first-child').style.marginLeft = `-${margin}px`;
     }
   }
@@ -59,7 +65,7 @@ module.exports = function slider(step, numbersSlide) {
     }
   }
 
-
+  document.querySelector('.slide_1 > input').setAttribute('class', 'active');
   document.querySelector('.btnSliderNext').addEventListener('click', nextSlide.bind(this, numbersSlide, step, stepCurrentSlide, margin));
   document.querySelector('.youtubeSlider').addEventListener('mousedown', handle.bind(this, numbersSlide, step, stepCurrentSlide, margin));
   document.querySelector('.youtubeSlider').addEventListener('mouseup', handle.bind(this, numbersSlide, step, stepCurrentSlide, margin));
