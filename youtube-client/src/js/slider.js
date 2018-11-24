1281cb5ee585d040b2ace809aa60b2b3eecb26ff
Roman Bychkov <romabrest1992@gmail.com>
2018-11-24 15:27:17 +0300

    :feat: add slider

--- youtube-client/src/js/slider.js
+++ youtube-client/src/js/slider.js
@@ -2,6 +2,7 @@ module.exports = function slider(step, numbersSlide) {
   let currentSlide = 1;
   const stepCurrentSlide = numbersSlide / numbersSlide;
   let margin = 0;
+  const coordinate = {};
   function nextSlide(numbersSlide) {
     if (currentSlide === numbersSlide) {
       currentSlide = 1;
@@ -13,5 +14,53 @@ module.exports = function slider(step, numbersSlide) {
       document.querySelector('.youtubeSlider > .videoSlide:first-child').style.marginLeft = `-${margin}px`;
     }
   }
+  function prevSlide(numbersSlide) {
+    if (currentSlide === 1) {
+      currentSlide = numbersSlide;
+      margin = step * (numbersSlide - 1);
+      document.querySelector('.youtubeSlider > .videoSlide:first-child').style.marginLeft = `-${margin}px`;
+    } else {
+      margin -= step;
+      currentSlide -= stepCurrentSlide;
+      document.querySelector('.youtubeSlider > .videoSlide:first-child').style.marginLeft = `-${margin}px`;
+    }
+  }
+  function handle() {
+    switch (event.type) {
+      case 'mousedown':
+        coordinate.startX = event.clientX;
+        coordinate.startY = event.clientY;
+        break;
+      case 'mouseup':
+        coordinate.endX = event.clientX;
+        coordinate.endY = event.clientY;
+        console.log(coordinate);
+        if (coordinate.startX > coordinate.endX) {
+          nextSlide(numbersSlide);
+        } else {
+          prevSlide(numbersSlide);
+        }
+        break;
+      case 'touchstart':
+        coordinate.startX = event.changedTouches[0].screenX;
+        coordinate.startY = event.changedTouches[0].screenY;
+        break;
+      case 'touchend':
+        coordinate.endX = event.changedTouches[0].screenX;
+        coordinate.endY = event.changedTouches[0].screenY;
+        console.log(coordinate);
+        if (coordinate.startX > coordinate.endX) {
+          nextSlide(numbersSlide);
+        } else {
+          prevSlide(numbersSlide);
+        }
+        break;
+      default: break;
+    }
+  }
+
+
   document.querySelector('.btnSliderNext').addEventListener('click', nextSlide.bind(this, numbersSlide, step, stepCurrentSlide, margin));
+  document.querySelector('.youtubeSlider').addEventListener('mousedown', handle.bind(this, numbersSlide, step, stepCurrentSlide, margin));
+  document.querySelector('.youtubeSlider').addEventListener('mouseup', handle.bind(this, numbersSlide, step, stepCurrentSlide, margin));
 };
