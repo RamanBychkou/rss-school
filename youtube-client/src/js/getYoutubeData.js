81b1cc898e014484080bf2c834e6668f7eb2e1b0
Roman Bychkov <romabrest1992@gmail.com>
2018-11-25 00:14:28 +0300

    refactor: create class Request

--- youtube-client/src/js/getYoutubeData.js
+++ youtube-client/src/js/getYoutubeData.js
@@ -1,8 +1,121 @@
 const changeWidth = require('./changeWidth.js');
-
 const CreateFragment = require('./createFragment.js');
 
+module.exports = class Request {
+  constructor(elementValue) {
+    this.data = undefined;
+    this.searchUrl = 'https://www.googleapis.com/youtube/v3/search?';
+    this.videoUrl = 'https://www.googleapis.com/youtube/v3/videos?';
+    this.commonSearchParametrs = {
+      part: 'snippet',
+      maxResults: 6,
+      q: elementValue,
+      type: 'video',
+      key: 'AIzaSyCni5hHJmCRuygcOBUiHGOdldAbRIOPQB8',
+    };
+    this.videoSearchParametrs = {
+      key: 'AIzaSyCni5hHJmCRuygcOBUiHGOdldAbRIOPQB8',
+      id: '',
+      part: 'snippet, statistics',
+    };
+  }
 
+  createUrl(url, defaultParams) {
+    let urlParametr = new URLSearchParams();
+    for (const key in defaultParams) {
+      urlParametr.append(key, defaultParams[key]);
+    }
+    urlParametr = urlParametr.toString();
+    console.log(url + urlParametr);
+    return url + urlParametr;
+  }
+
+  dataProcessing(dataV) {
+    const result = {};
+    const idArray = [];
+    this.commonSearchParametrs.pageToken = dataV.nextPageToken;
+    for (let i = 0; i < dataV.items.length; i += 1) {
+      result[dataV.items[i].id.videoId] = {
+        title: dataV.items[i].snippet.title,
+        date: dataV.items[i].snippet.publishedAt,
+        id: dataV.items[i].id.videoId,
+        description: dataV.items[i].snippet.description,
+        pictureSmall: dataV.items[i].snippet.thumbnails.default.url,
+        picture: dataV.items[i].snippet.thumbnails.medium.url,
+        author: dataV.items[i].snippet.channelTitle,
+      };
+      idArray.push(dataV.items[i].id.videoId);
+    }
+    this.data = result;
+    return idArray;
+  }
+
+  viewCountData(dataV) {
+    for (let i = 0; i < dataV.items.length; i += 1) {
+      this.data[dataV.items[i].id].ViewCounts = dataV.items[i].statistics.viewCount;
+    }
+    return this.show();
+  }
+
+
+  getRequest() {
+    fetch(this.createUrl(this.searchUrl, this.commonSearchParametrs))
+      .then(async (response) => {
+        const data = await response.json();
+        const id = this.dataProcessing(data);
+        for (let i = 0; i < 6; i += 1) {
+          if (i !== 5) {
+            this.videoSearchParametrs.id = `${this.videoSearchParametrs.id + id[i]},`;
+          } else {
+            this.videoSearchParametrs.id = this.videoSearchParametrs.id + id[i];
+          }
+        }
+        return this.videoRequest();
+      }).then(response => Promise.resolve(response));
+  }
+
+  videoRequest() {
+    fetch(this.createUrl(this.videoUrl, this.videoSearchParametrs))
+      .then(async (response) => {
+        const secondData = await response.json();
+        this.viewCountData(secondData);
+      })
+      .then(response => Promise.resolve(response));
+  }
+
+  getData() { return this.data; }
+
+  show(data) {
+    if(document.querySelector('.youtubeContainer') !== null) {
+      document.querySelector('.youtubeContainer').remove();
+    }
+    const showVideo = new CreateFragment();
+    showVideo.createElement('div').setAttr({ class: 'youtubeContainer' }).setInFragment(null);
+    showVideo.createElement('div').setAttr({ class: 'youtubeSlider' }).setInFragment('.youtubeContainer');
+    showVideo.createElement('button').setText('next').setAttr({ class: 'btnSliderNext' }).setInFragment('.youtubeContainer');
+    showVideo.setInDocument('.container');
+    for (const key in this.data) {
+      const tempVideo = new CreateFragment();
+      tempVideo.createElement('div').setAttr({ class: 'videoSlide' }).setInFragment(null);
+      tempVideo.createElement('div').setAttr({ class: 'videoImg' }).setInFragment('.videoSlide');
+      tempVideo.createElement('img').setAttr({ src: this.data[key].picture, alt: this.data[key].title }).setInFragment('.videoImg');
+      tempVideo.createElement('div').setAttr({ class: 'videoInfo' }).setInFragment('.videoSlide');
+      tempVideo.createElement('ul').setInFragment('.videoInfo');
+      tempVideo.createElement('li').setInFragment('ul');
+      tempVideo.createElement('a').setAttr({ href: `https://www.youtube.com/watch?v=${this.data[key].id}` }).setText(`Title: ${this.data[key].title}`).setInFragment('ul > li');
+      tempVideo.createElement('li').setText(`Date: ${this.data[key].date}`).setInFragment('ul');
+      tempVideo.createElement('li').setText(`Author: ${this.data[key].author}`).setInFragment('ul');
+      tempVideo.createElement('li').setText(`description: ${this.data[key].description}`).setInFragment('ul');
+      tempVideo.createElement('li').setText(`View Count: ${this.data[key].ViewCounts}`).setInFragment('ul');
+      tempVideo.setInDocument('.youtubeSlider');
+    }
+    changeWidth();
+    window.addEventListener('resize', changeWidth);
+  }
+};
+
+
+/*
 module.exports = function getRequest(e) {
   const elementValue = document.querySelector('input').value;
   let params = new URLSearchParams();
@@ -44,7 +157,7 @@ module.exports = function getRequest(e) {
     }
     const clearData = checkData(data);
     console.log(clearData);
-    
+
     function show(data) {
       if(document.querySelector('.youtubeContainer') !== null) {
         document.querySelector('.youtubeContainer').remove();
@@ -76,4 +189,4 @@ module.exports = function getRequest(e) {
     console.log(error);
   });
   return info;
-}; 
+}; */
--- youtube-client/src/js/index.js
+++ youtube-client/src/js/index.js
@@ -5,20 +5,23 @@ const getRequest = require('./getYoutubeData.js');
 // import data from './getYoutubeData.js'
 
 window.onload = () => {
-  
   function getSearch(e) {
     e.preventDefault();
-    const data = getRequest(e);
-    console.log(data);
+    const elementValue = document.querySelector('input').value;
+    const getData = new getRequest(elementValue);
+    getData.getRequest();
+
+
+    // const data = getRequest(e);
   }
-  let showSearchField = new CreateFragment();
-  showSearchField.createElement('div').setAttr({class : 'container'}).setInFragment(null);
-  showSearchField.createElement('div').setAttr({class : 'searchField'}).setInFragment('.container');
+  const showSearchField = new CreateFragment();
+  showSearchField.createElement('div').setAttr({ class: 'container' }).setInFragment(null);
+  showSearchField.createElement('div').setAttr({ class: 'searchField' }).setInFragment('.container');
   showSearchField.createElement('h1').setText('Youtube Searh App').setInFragment('.searchField');
   showSearchField.createElement('form').setInFragment('.searchField');
-  showSearchField.createElement('input').setAttr({type: 'text', placeholder : 'Search me'} ).setInFragment('form');
-  showSearchField.createElement('button').setAttr({class : 'btn-search'}).setText('Search').setInFragment('form');
-  showSearchField.setInDocument('body')
+  showSearchField.createElement('input').setAttr({ type: 'text', placeholder: 'Search me' }).setInFragment('form');
+  showSearchField.createElement('button').setAttr({ class: 'btn-search' }).setText('Search').setInFragment('form');
+  showSearchField.setInDocument('body');
 
   document.querySelector('.btn-search').addEventListener('click', getSearch);
 };
