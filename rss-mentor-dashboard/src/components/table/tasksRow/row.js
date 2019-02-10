894f26c7b34d671f1bc981e2cc479e05942d86fa
Roman Bychkov <romabrest1992@gmail.com>
2019-02-10 16:49:57 +0300

    feat: add task marks on the page

--- rss-mentor-dashboard/src/components/table/tasksRow/row.js
+++ rss-mentor-dashboard/src/components/table/tasksRow/row.js
@@ -5,33 +5,55 @@ const data = require('../../../data.json');
 class TasksRow extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       tasks: data.taskInfo,
-      mentor: 'Aliaksandr Zayats',
-     };
+      mentor: 'Dzianis Sheka',
+      students: data['Dzianis Sheka'].tasks.students,
+      taskMarks: data['Dzianis Sheka'].tasks,
+    };
   }
 
   render() {
     const tasks = this.state.tasks;
-    const taskRow = tasks.map((current, index) => (
-      <tr>
-        <td>
-        {current.name}
-        </td>
-      </tr>
-      
-    ));
+    const taskRow = tasks.map((current, index) => {
+      let marks = [];
+      debugger;
+      console.log(current.name);
+      for (const key in this.state.taskMarks[current.name]) {
+        marks.push(this.state.taskMarks[current.name][key]);
+      }
+      marks = marks.map((current) => (
+          <td>{current}</td>
+        ));
 
-    const firstRow = (data) => {
-      const menorData = data[this.state[mentor]];
+      return (
+        <tr>
+        <td>
+          {current.name}
+        </td>
+        {marks}
+      </tr>
+
+      )
+; });
+
+    const studentsCell = [];
+    for (const key in this.state.students) {
+      const student = (
+        <th>{this.state.students[key]}</th>
+      );
+      studentsCell.push(student);
     }
 
     return (
       <tbody id="table">
-        
+        <tr>
+          <th>{this.state.mentor}</th>
+          {studentsCell}
+        </tr>
         { taskRow }
       </tbody>
-        
+
     );
   }
 }
