e9e84c013c66e9a6d218b44cc6328f17b4f891da
Roman Bychkov <romabrest1992@gmail.com>
2019-03-10 23:48:57 +0300

    docs: delete game folder

--- game/src/components/tasks/footbal/football.tasks.js
+++
@@ -1,8 +0,0 @@
-const tasks = [
-  'Who won the Champions League in 2018?',
-  'Who are the players moved to Juventus in the summer of 2018?',
-  'Which of the tournaments is not international?',
-  'Which players got the golden ball in 2018?',
-  'Which country won the world championship in 2018?',
-];
-export default tasks;
